18b65aa6ba47871c5d675358704f0302e17ebcd7
bkmzc <bkmzc676@gmail.com>
2024-09-18 16:21:53 +0300

    ghdhhdgdhfgdg

---
+++ task/config_task.py
@@ -0,0 +1,22 @@
+
+RPC_URL =  "https://rpc.bitlayer.org"
+
+# ID задач для клейма (можно добавить больше)
+TASK_IDS = [
+    {"id": 9, "type": 3},
+    {"id": 23, "type": 3},
+    {"id": 24, "type": 0},
+    {"id": 7, "type": 2},
+    # ...
+]
+
+
+
+#type = 1 - дейлик 2 - new rt 3 - at
+# FB - id: 6 type 2
+# 10 транзы id 16 type 3
+# id 24 за каждую транзу
+# id 9 type 3      - AT 1
+# 23 3  - 20 trn
+# 7 2 - send frend     8 2  rec frend
+
